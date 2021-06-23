925aab7a8bf27d611d9aea1a69c836778b8bd6f5
Johanna <86261642+johago521@users.noreply.github.com>
2021-06-23 16:53:18 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,13 +2,8 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!!");
     }
-
 }
-
-
 //This is Mahmood's branch "Branch1"
-
     //asad putting stuff
-}
 //Mahmood's Contribution
 
